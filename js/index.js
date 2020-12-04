ddd287cbdfc1f122a943772305c7ddf7b15f21bd
Ganesh Balaji Radhakrishnan <ganesh.krishnan237@gmail.com>
2020-12-04 16:21:27 +0100

    changes in card and color in about me section

--- js/index.js
+++ js/index.js
@@ -1,12 +1,27 @@
-const navToggle = document.querySelector('.nav-toggle');
+const navToggle = document.querySelector('.nav-toggle')
 const navLinks = document.querySelectorAll('.nav__link')
 
 navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
-
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
+  document.body.classList.toggle('nav-open')
 })
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open')
+  })
+})
+
+const panelBox = document.querySelectorAll('.panel')
+
+panelBox.forEach((pan) => {
+  pan.addEventListener('click', () => {
+    removeActiveClass()
+    pan.classList.add('active')
+  })
+})
+
+function removeActiveClass() {
+  panelBox.forEach((pan) => {
+    pan.classList.remove('active')
+  })
+}
